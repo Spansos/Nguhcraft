604f25a0f4588db1def9d45201449b8fe2f99b0c
Spansos <spaansstan@gmail.com>
2026-05-12 05:18:37 +0200

    hoppers with same lock as their containers work now

--- src/main/java/org/nguh/nguhcraft/mixin/protect/server/HopperBlockEntityMixin.java
+++ src/main/java/org/nguh/nguhcraft/mixin/protect/server/HopperBlockEntityMixin.java
@@ -3,10 +3,12 @@ package org.nguh.nguhcraft.mixin.protect.server;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.entity.Hopper;
 import net.minecraft.world.level.block.entity.HopperBlockEntity;
+import net.minecraft.world.entity.vehicle.MinecartHopper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
-import org.nguh.nguhcraft.protect.ProtectionManager;
+import org.nguh.nguhcraft.item.KeyItem;
+import org.nguh.nguhcraft.item.LockableBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +18,7 @@ import java.util.function.BooleanSupplier;
 
 @Mixin(HopperBlockEntity.class)
 public abstract class HopperBlockEntityMixin {
-    /** As below. Used by hopper minecarts. */
+    /** As below. Used also by hopper minecarts. */
     @Inject(
         method = "suckInItems(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/entity/Hopper;)Z",
         at = @At("HEAD"),
@@ -24,7 +26,10 @@ public abstract class HopperBlockEntityMixin {
     )
     private static void inject$extract(Level W, Hopper H, CallbackInfoReturnable<Boolean> CIR) {
         BlockPos Pos = BlockPos.containing(H.getLevelX(), H.getLevelY() + 1.0, H.getLevelZ());
-        if (ProtectionManager.IsProtectedBlock(W, Pos))
+        LockableBlockEntity LBE = KeyItem.GetLockableEntity(W, Pos);
+        // minecart hopper can never interact with any locked containers.
+        // only the minecart case needs to be handled here, normal hoppers only call this function from insertAndExtract
+        if (H instanceof MinecartHopper && LBE != null && LBE.Nguhcraft$GetLock() != null)
             CIR.setReturnValue(false);
     }
 
@@ -37,9 +42,6 @@ public abstract class HopperBlockEntityMixin {
     * from/to, we need to perform this check early, instead of doing the
     * SENSIBLE thing and simply returning null for the input and output
     * inventories.
-    * <p>
-    * TODO: Allow locking hoppers and allow locked hoppers to
-    *       access protected inventories with the same key.
     */
     @Inject(
         method = "tryMoveItems",
@@ -59,9 +61,17 @@ public abstract class HopperBlockEntityMixin {
         // if a hopper is touching a protected block, then fuck you, nothing is going
         // to move anywhere.
         Direction Facing = ((HopperBlockEntityAccessor) BE).getFacing();
+        
+        // continue only if insert container has the same lock (or both are null)
         BlockPos ToPos = Pos.relative(Facing);
+        LockableBlockEntity ToBE = KeyItem.GetLockableEntity(W, ToPos);
+        if (ToBE != null && ((LockableBlockEntity) BE).Nguhcraft$GetLock() != ToBE.Nguhcraft$GetLock())
+            CIR.setReturnValue(false);
+        
+        // continue only if extract container has the same lock (or both are null)
         BlockPos FromPos = Pos.above();
-        if (ProtectionManager.IsProtectedBlock(W, FromPos) || ProtectionManager.IsProtectedBlock(W, ToPos))
+        LockableBlockEntity FromBE = KeyItem.GetLockableEntity(W, FromPos);
+        if (FromBE != null && ((LockableBlockEntity) BE).Nguhcraft$GetLock() != FromBE.Nguhcraft$GetLock())
             CIR.setReturnValue(false);
     }
 }
